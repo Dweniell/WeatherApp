aa8648866ba2ab526adbb0d7da2dacf5abd2f325
Andrei <105932208+Dweniell@users.noreply.github.com>
2024-10-03 21:41:51 +0300

    Delete WeatherApp/Task2 directory
    -removed task2

--- WeatherApp/Task2/firstReport.sql
+++
@@ -1,1 +0,0 @@
-SELECT p.Locatie,p.First_Name,p.Last_Name,v.quality FROM votes v,persons p WHERE v.chosen_person = p.id AND v.valid=1;
--- WeatherApp/Task2/secondReport.sql
+++
@@ -1,1 +0,0 @@
-SELECT p.Locatie,count(v.id) as votes from persons p left join votes v on p.id = v.chosen_person and v.valid=1 group by p.Locatie;
